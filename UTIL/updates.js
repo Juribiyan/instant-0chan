7b6f8e5f20e33d6ef6d624815ae26c70095e48a6
Juribiyan <smugleaf@ya.ru>
2014-07-16 19:40:08 +0600

    node.js code for board page replies indicator
    (забыл про него)

--- UTIL/updates.js
+++ UTIL/updates.js
@@ -13,13 +13,22 @@ io.set('browser client minification', true);
 
 io.sockets.on('connection', function (socket) {
     socket.on('srvmsg', function(data) {
-    	console.log(data);
     	if(typeof data.srvtoken === 'undefined' || data.srvtoken !== srvtoken || typeof data.room === 'undefined') return;
-        io.sockets.in(data.room).emit('update', {token: data.clitoken, timestamp: data.timestamp});
+        io.sockets.in(data.room).emit('update', {token: data.clitoken, timestamp: data.timestamp, room: data.room.split(':')[1]});
     });
-    socket.on('subscribe', function(room) {
-    	socket.join(room);
+    socket.on('subscribe', function(rooms) {
+    	iter(rooms, function(room) {
+    		socket.join(room)
+    	});
     });
 });
+
+function iter(array, callback) {
+    if(typeof array !== 'object') return callback(array);
+    var i=0, len = array.length;
+    for ( ; i < len ; i++ ) {
+        callback(array[i]);
+    }
+} 
 
 server.listen(node_port, node_ip);
