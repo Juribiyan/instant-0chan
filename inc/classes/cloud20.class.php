873fb7d21312801f72b3f8470d72a580f60631cf
Juribiyan <snivy@ponyasha.ru>
2015-10-11 18:25:26 +0500

    JSON boardlist building for default boards

--- inc/classes/cloud20.class.php
+++ inc/classes/cloud20.class.php
@@ -1,17 +1,27 @@
 <?
 class Cloud20 {
 	function rebuild() {
-		global $tc_db;
+	  global $tc_db;
 
-		$bjson = KU_ROOTDIR . '/boards20.json';
+	  $sections = $tc_db->GetAll('SELECT * FROM `'. KU_DBPREFIX .'sections`');
 
-		$boardid20 = $tc_db->GetOne('SELECT `id` FROM `'. KU_DBPREFIX .'sections` WHERE `abbreviation`="20"');
-		$boards20 = $tc_db->GetAll('SELECT `id`, `name`, `desc` FROM `'. KU_DBPREFIX .'boards` WHERE `section` = '.$boardid20.' ORDER BY `name` ASC');
-		foreach($boards20 as &$b20) {
-			$postcount = $tc_db->GetOne('SELECT COUNT(1) FROM `'. KU_DBPREFIX .'posts` WHERE `boardid`='.$b20['id'].' AND `timestamp` >= '.strtotime(KU_20_CLOUDTIME));
-			$results[] = array('name'=>$b20['name'], 'desc'=>$b20['desc'], 'postcount'=>$postcount);
-		}unset($b20);
+	  $allboards = $tc_db->GetAll('SELECT `id`, `name`, `desc`, `section` FROM `'. KU_DBPREFIX .'boards` ORDER BY `name` ASC');
+	  $section20 = $tc_db->GetOne('SELECT `id` FROM `'. KU_DBPREFIX .'sections` WHERE `abbreviation`="20"');
+	  foreach($sections as &$sect) {
+	    $sect_map[$sect['id']] = $sect['abbreviation'];
+	  }unset($sect);
 
-		file_put_contents($bjson, json_encode($results), LOCK_EX);
+	  foreach($allboards as &$board) {
+	    if($sect_map[$board['section']] === "20") {
+	      $postcount = $tc_db->GetOne('SELECT COUNT(1) FROM `'. KU_DBPREFIX .'posts` WHERE `boardid`='.$board['id'].' AND `timestamp` >= '.strtotime(KU_20_CLOUDTIME));
+	      $boards20 []= array('name'=>$board['name'], 'desc'=>$board['desc'], 'postcount'=>$postcount);
+	    }
+	    elseif(isset($sect_map[$board['section']])) {
+	      $boards10[$sect_map[$board['section']]] []= array('name'=>$board['name'], 'desc'=>$board['desc']);
+	    }
+	  }unset($board);
+
+	  file_put_contents(KU_ROOTDIR . '/boards10.json', json_encode($boards10), LOCK_EX);
+	  file_put_contents(KU_ROOTDIR . '/boards20.json', json_encode($boards20), LOCK_EX);
 	}
 }
