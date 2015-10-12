0ca7c733290d3afc2e439e3a4b050e27982fa248
Juribiyan <snivy@ponyasha.ru>
2015-10-12 11:09:45 +0600

    Improvements for prev. commit

--- inc/classes/cloud20.class.php
+++ inc/classes/cloud20.class.php
@@ -3,12 +3,14 @@ class Cloud20 {
 	function rebuild() {
 	  global $tc_db;
 
-	  $sections = $tc_db->GetAll('SELECT * FROM `'. KU_DBPREFIX .'sections`');
+	  $sections = $tc_db->GetAll('SELECT * FROM `'. KU_DBPREFIX .'sections` ORDER BY `order`');
 
 	  $allboards = $tc_db->GetAll('SELECT `id`, `name`, `desc`, `section` FROM `'. KU_DBPREFIX .'boards` ORDER BY `name` ASC');
 	  $section20 = $tc_db->GetOne('SELECT `id` FROM `'. KU_DBPREFIX .'sections` WHERE `abbreviation`="20"');
 	  foreach($sections as &$sect) {
-	    $sect_map[$sect['id']] = $sect['abbreviation'];
+	    $sect_map[$sect['id']] = $sect['name'];
+	    if($sect['abbreviation'] !== '20')
+	    	$boards10_wrap[$sect['id']] = array(order=>$sect['order'], name=>$sect['name']);
 	  }unset($sect);
 
 	  foreach($allboards as &$board) {
@@ -17,11 +19,15 @@ class Cloud20 {
 	      $boards20 []= array('name'=>$board['name'], 'desc'=>$board['desc'], 'postcount'=>$postcount);
 	    }
 	    elseif(isset($sect_map[$board['section']])) {
-	      $boards10[$sect_map[$board['section']]] []= array('name'=>$board['name'], 'desc'=>$board['desc']);
+	      $boards10_wrap[$board['section']]['boards'] []= array('dir'=>$board['name'], 'desc'=>$board['desc']);
 	    }
 	  }unset($board);
 
+	  foreach ($boards10_wrap as $key => $value) {
+	  	$boards10 []= $value;
+	  }
+
 	  file_put_contents(KU_ROOTDIR . '/boards10.json', json_encode($boards10), LOCK_EX);
-	  file_put_contents(KU_ROOTDIR . '/boards20.json', json_encode($boards20), LOCK_EX);
+		file_put_contents(KU_ROOTDIR . '/boards20.json', json_encode($boards20), LOCK_EX);
 	}
 }
