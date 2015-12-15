97ba9f7540dae68d8d27f7db96a47611ae9f3b9a
Juribiyan <snivy@ponyasha.ru>
2015-12-15 22:04:57 +0500

    Fixed unprefixed newpostcount

--- newpostscount.php
+++ newpostscount.php
@@ -2,12 +2,12 @@
 
 require 'config.php';
 
-$brds = $tc_db->GetAll('SELECT `id`,`name` FROM `boards`;');
+$brds = $tc_db->GetAll('SELECT `id`,`name` FROM `'.KU_DBPREFIX.'boards`');
 
 foreach ($brds as &$brd) {
     if(isset($_GET[$brd["id"]])) $last_ts = $_GET[$brd["id"]];
     else $last_ts = 0;
-    $result[$brd["name"]] = $tc_db->GetOne('SELECT COUNT(1) FROM `posts` WHERE `boardid` = '.$brd["id"].' AND`timestamp` > '.$last_ts);
+    $result[$brd["name"]] = $tc_db->GetOne('SELECT COUNT(1) FROM `'.KU_DBPREFIX.'posts` WHERE `boardid` = '.$brd["id"].' AND`timestamp` > '.$last_ts);
 }
 unset($brd);
 
