86eff9b8b05c4291e3a73fcbeea62d1971d96557
Juribiyan <snivy@ponyasha.ru>
2018-07-09 17:07:42 +0500

    Fucking linux with its case sensitivity

--- getpreferredstylesheet.php
+++ getpreferredstylesheet.php
@@ -6,7 +6,7 @@ $style = $_COOKIE['kustyle'];
 if (!$style)
 	$style = KU_DEFAULTSTYLE;
 
-redirect(KU_WEBPATH.KU_BOARDSFOLDER.'css/'.$style.'.css?v='.KU_CSSVER);
+redirect(KU_WEBPATH.KU_BOARDSFOLDER.'css/'.strtolower($style).'.css?v='.KU_CSSVER);
 
 function redirect($url, $statusCode = 303) {
 	header('Location: ' . $url, true, $statusCode);
