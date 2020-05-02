352929dc1a48ec1101b0acbdde697a7ed6050e03
devarped <devar@redchan.it>
2020-05-03 02:02:17 +0300

    Forgot the instance-config itself

---
+++ instance-config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This is the file for your custom settings.
+ * If you copy settings form config.php, don't forget to check if there are any other settings depending on whatever settings you change.
+ */
+
+// Database
+$cf['KU_DBTYPE']          = 'mysqli';	// Database type. Valid values are mysql and mysqli (reccomended for mysql).
+$cf['KU_DBHOST']          = 'localhost'; // Database hostname. On SQLite this has no effect.
+$cf['KU_DBDATABASE']      = '0chan'; // Database... database. On SQLite this will be the path to your database file. Secure this file.
+$cf['KU_DBUSERNAME']      = 'root'; // Database username. On SQLite this has no effect.
+$cf['KU_DBPASSWORD']      = ''; // Database password. On SQLite this has no effect.
+$cf['KU_DBPREFIX']        = ''; // Database table prefix
+
+$cf['KU_WEBPATH']   = '//example.com'; // The path to the index folder of kusaba, without trailing slash. (http://yoursite.com or https://yoursite.com or just //yoursite.com for protocol-agnostic (flexible) behavior)
+$cf['KU_DOMAIN']    = '.example.com'; // Used in cookies for the domain parameter.  Should be a period and then the top level domain, which will allow the cookies to be set for all subdomains.
+
+// Board subdomain/alternate directory (optional, change to enable)
+// DO NOT CHANGE THESE IF YOU DO NOT KNOW WHAT YOU ARE DOING!!
+$cf['KU_BOARDSDIR']    = $cf['KU_ROOTDIR'];
+$cf['KU_BOARDSFOLDER'] = $cf['KU_WEBFOLDER'];
+$cf['KU_BOARDSPATH']   = $cf['KU_WEBPATH'];
+
+// CGI subdomain/alternate directory (optional, change to enable)
+// DO NOT CHANGE THESE IF YOU DO NOT KNOW WHAT YOU ARE DOING!!
+$cf['KU_CGIDIR']    = $cf['KU_BOARDSDIR'];
+$cf['KU_CGIFOLDER'] = $cf['KU_BOARDSFOLDER'];
+$cf['KU_CGIPATH']   = $cf['KU_BOARDSPATH'];
+
+// Misc config
+$cf['KU_MODLOGDAYS']        = 7; // Days to keep modlog entries before removing them
+$cf['KU_RANDOMSEED']        = 'ENTERRANDOMSHIT'; // Type a bunch of random letters/numbers here, any large amount (35+ characters) will do
