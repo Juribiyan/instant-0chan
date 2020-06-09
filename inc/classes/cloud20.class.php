1dc23acbb712eb92983e962e83aeeb49e59be8d5
Juribiyan <smugleaf@ya.ru>
2020-06-09 23:15:28 +0500

    Optimize boards JSON rebuilding to a single SQL query

--- inc/classes/cloud20.class.php
+++ inc/classes/cloud20.class.php
@@ -1,33 +1,34 @@
 <?php
 class Cloud20 {
 	function rebuild() {
-	  global $tc_db;
+		global $tc_db;
 
-	  $sections = $tc_db->GetAll('SELECT * FROM `'. KU_DBPREFIX .'sections` ORDER BY `order`');
-	  $allboards = $tc_db->GetAll('SELECT `id`, `name`, `desc`, `section`, `order` FROM `'. KU_DBPREFIX .'boards` WHERE `hidden` != 1');
-	  $section20 = $tc_db->GetOne('SELECT `id` FROM `'. KU_DBPREFIX .'sections` WHERE `abbreviation`="20"');
+		$all = $tc_db->GetAll('SELECT `b`.`name`, `desc`, `b`.`order`, `pc`.`postcount`, `sect`.`order` as `sectorder`, `sect`.`name` as `sect`, `is_20` FROM
+			(SELECT * FROM `'.KU_DBPREFIX.'boards`) `b`
+			JOIN (SELECT *, (`abbreviation`="20") AS `is_20` FROM `'.KU_DBPREFIX.'sections`) `sect`
+			ON `b`.`section` = `sect`.`id`
+			LEFT JOIN(SELECT `boardid`, COUNT(1) AS `postcount` FROM `'.KU_DBPREFIX.'posts` GROUP BY `boardid`) `pc`
+			ON `b`.`id` = `pc`.`boardid`
+			WHERE `b`.`hidden` != 1');
 
-	  foreach($sections as &$sect) {
-	    $sect_map[$sect['id']] = $sect['abbreviation'];
-	    if($sect['abbreviation'] !== '20')
-	    	$boards10_wrap[$sect['id']] = array('order'=>$sect['order'], 'name'=>$sect['name']);
-	  }unset($sect);
+		foreach($all as &$board) {
+			if ($board['is_20']) {
+				$boards20 []= array('name'=>$board['name'], 'desc'=>$board['desc'], 'postcount'=>($board['postcount'] ? (int)$board['postcount'] : 0));
+			}
+			else {
+				if (!isset($boards10_wrap[$board['sect']]['name'])) {
+					$boards10_wrap[$board['sect']]['name'] = $board['sect'];
+					$boards10_wrap[$board['sect']]['order'] = $board['sectorder'];
+				}
+				$boards10_wrap[$board['sect']]['boards'] []= array('dir'=>$board['name'], 'desc'=>$board['desc'], 'order'=>$board['order']);
+			}
+		}unset($board);
 
-	  foreach($allboards as &$board) {
-	    if($sect_map[$board['section']] === "20") {
-	      $postcount = $tc_db->GetOne('SELECT COUNT(1) FROM `'. KU_DBPREFIX .'posts` WHERE `boardid`='.$board['id'].' AND `timestamp` >= '.strtotime(KU_20_CLOUDTIME));
-	      $boards20 []= array('name'=>$board['name'], 'desc'=>$board['desc'], 'postcount'=>$postcount);
-	    }
-	    elseif(isset($sect_map[$board['section']])) {
-	      $boards10_wrap[$board['section']]['boards'] []= array('dir'=>$board['name'], 'desc'=>$board['desc'], 'order'=>$board['order']);
-	    }
-	  }unset($board);
+		foreach ($boards10_wrap as $key => $value) {
+			$boards10 []= $value;
+		}
 
-	  foreach ($boards10_wrap as $key => $value) {
-	  	$boards10 []= $value;
-	  }
-
-	  file_put_contents(KU_ROOTDIR . '/boards10.json', json_encode($boards10), LOCK_EX);
+		file_put_contents(KU_ROOTDIR . '/boards10.json', json_encode($boards10), LOCK_EX);
 		file_put_contents(KU_ROOTDIR . '/boards20.json', json_encode($boards20), LOCK_EX);
 	}
 }
